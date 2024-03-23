e23e46a1629489285c69ad357d3347c87034cd94
VedikaBasarkar <vedika.basarkar30@gmail.com>
2024-03-23 20:35:22 +0530

    Update for padding

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 text-orange-600">
+    <main className="flex md:min-h-screen flex-col items-center justify-between md:px-24 text-orange-600">
       <img className="" src="https://design-habitat.nyc3.cdn.digitaloceanspaces.com/illustrations/coming-soon.png" /> 
     </main>
   );
